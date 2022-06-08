2e7096e77a7207eb86052cf9a071580607cacf98
evisp <evisplaku@gmail.com>
2022-06-08 06:28:35 -0700

    Add: insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list of ints
+ * @list: a double linked list
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+  listint_t *current = NULL;
+
+  if (list == NULL)
+    return;
+
+  current = (*list)->next;
+  while (current != NULL)
+    {
+      while (current->prev != NULL && current->prev->n > current->n)
+	{
+	  current->prev->next = current->next;
+	  if (current->next != NULL)
+	    current->next->prev = current->prev;
+
+	  current->next = current->prev;
+	  current->prev = current->next->prev;
+	  current->next->prev = current;
+
+	  if (current->prev == NULL)
+	    *list = current;
+	  else
+	    current->prev->next = current;
+
+	  print_list(*list);
+	}
+      current = current->next;
+    }
+}
